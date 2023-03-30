ac63d483d5d3cb3e1e158602fff6f43c6bb2293c
nkurudavid <nkurudavid44@gmail.com>
2023-03-31 00:43:49 +0200

    configured media directory for files

--- backend/urls.py
+++ backend/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('renting.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 admin.site.site_title = "Admin panel"
 admin.site.site_header = "Kigali House Renting System"
