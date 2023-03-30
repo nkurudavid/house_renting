7512eef17531e37431d77ea0c9fc0a5f4e75b6d1
nkurudavid <nkurudavid44@gmail.com>
2023-03-31 00:45:07 +0200

    media configs

--- renting/models.py
+++ renting/models.py
@@ -1,7 +1,83 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.safestring import mark_safe
+from django.core.validators import FileExtensionValidator
+
 
 # get user model
 User = get_user_model()
 
 # Create your models here.
+class Manager(models.Model):
+    class Gender(models.TextChoices):
+        SELECT = "", "Select Gender"
+        MALE = "Male", "Male"
+        FEMALE = "Female", "Female"
+
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    gender = models.CharField(verbose_name="Gender", choices=Gender.choices, default=Gender.SELECT, max_length=10)
+    phone_number = PhoneNumberField(verbose_name = "Phone Number",blank=True)
+    location = models.CharField(verbose_name="Location Address", max_length=255, blank=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.user.first_name,self.user.last_name)
+
+
+
+class Landlord(models.Model):
+    class Gender(models.TextChoices):
+        SELECT = "", "Select Gender"
+        MALE = "Male", "Male"
+        FEMALE = "Female", "Female"
+
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    gender = models.CharField(verbose_name="Gender", choices=Gender.choices, default=Gender.SELECT, max_length=10)
+    phone_number = PhoneNumberField(verbose_name = "Phone Number",blank=True)
+    location = models.CharField(verbose_name="Location Address", max_length=255, blank=True)
+    profile_image = models.ImageField(
+        verbose_name="Profile Picture", 
+        upload_to='', 
+        height_field=None, 
+        width_field=None, 
+        max_length=None,
+        validators=[FileExtensionValidator(['png','jpg','jpeg'])]
+    )
+    
+    def  image(self):
+        return mark_safe('<img src="/../../media/%s" width="70" />' % (self.profile_image))
+
+    image.allow_tags = True 
+    
+    def __str__(self):
+        return '{} {}'.format(self.user.first_name,self.user.last_name)
+
+
+class City(models.Model):
+    name = models.CharField(verbose_name="City/District", max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PropertyType(models.Model):
+    name = models.CharField(verbose_name="Property Type", max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Property(models.Model):
+    landlord = models.ForeignKey(Landlord, verbose_name="Landlord", on_delete=models.CASCADE, blank=False)
+    city = models.ForeignKey(City, verbose_name="City/District", on_delete=models.PROTECT)
+    type = models.ForeignKey(PropertyType, verbose_name="Property Type", on_delete=models.CASCADE)
+    locationAddress = models.CharField(verbose_name="Location Address", max_length=255, blank=False)
+    description
+    bedrooms
+    bathrooms
+    is_furnished
+    floors
+    plot_size
+    renting_price
+    status
+    pub_date = models.DateTimeField(auto_now_add=True)
+    created_date
